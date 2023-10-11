36f8daba423c2317f3bf24f11655a8c8f2ee436d
final_graf <fhicdnb@gmail.com>
2023-10-11 19:07:18 +0300

    first

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -3,42 +3,32 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class DrawingPanel extends JFrame implements
-        ActionListener {
+class DrawingPanel extends JFrame {
     private final int BACKGROUND_WIDTH = 1200;
     private final int BACKGROUND_HEIGHT = 800;
-
-    private final int TIMER_DELAY = 500;
-    private final Timer timer = new Timer ( TIMER_DELAY ,
-            this );
 
     public DrawingPanel() {
         setTitle (" Drawing panel ");
         setSize ( BACKGROUND_WIDTH , BACKGROUND_HEIGHT );
         setVisible ( true );
         setDefaultCloseOperation ( DISPOSE_ON_CLOSE ) ;
-      timer . start () ;
     }
 
     @Override
     public void paint ( Graphics g) {
         Graphics2D g2d = ( Graphics2D ) g;
 
-        g2d . setColor ( Color . green ) ;
+        g2d . setColor ( Color . decode("#4cbb17") ) ;
         g2d . fillRect (0 , 0, BACKGROUND_WIDTH,
                 BACKGROUND_HEIGHT) ;
 
-        g2d . setColor ( Color . BLUE ) ;
+        g2d . setColor ( Color . decode("#bfefff") ) ;
         g2d . fillRect (0 , 0, BACKGROUND_WIDTH,
                 BACKGROUND_HEIGHT / 2 + 100) ;
 
         new fly().draw(g2d, 10, 500);
     }
-
-    @Override
-    public void actionPerformed ( ActionEvent e) {
-        if ( e. getSource () == timer ) {
-            repaint () ;
-        }
-    }
 }
+
+
+//захуярить все в интерфейс все приколы в лист
